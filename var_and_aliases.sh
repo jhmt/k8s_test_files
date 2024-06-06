d3caf13973dcf601edb791271d7f8aee4d237a4d
Jun Hashimoto <jun.hashimoto211@gmail.com>
2024-06-06 20:41:44 +1200

    Update var_and_aliases.sh

--- var_and_aliases.sh
+++ var_and_aliases.sh
@@ -3,5 +3,7 @@ export do="--dry-run=client -o yaml"
 alias kc="kubectl create"
 alias ka="kubectl apply -f"
 alias kr="kubectl replace --force -f"
+alias kn="kubectl config set-context --current --namespace"
+
 
 alias debug="kubectl run -it --rm --image=curlimages/curl c -- sh"
