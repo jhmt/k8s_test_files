0629e800b2229b1b3ece870d0aa563184ca6cad4
Jun Hashimoto <jun.hashimoto211@gmail.com>
2024-06-13 23:42:06 +1200

    Update hello_world.sh

--- hello_world.sh
+++ hello_world.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace > helm_result.txt
 
 NODE_IP=$(kubectl get nodes controlplane -o jsonpath='{.status.addresses[?(@.type=="InternalIP")].address}')
 echo $NODE_IP hello.com >> /etc/hosts
 
+echo "Ingress is ready!"
